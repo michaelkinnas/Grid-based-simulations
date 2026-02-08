0f6cefa66adb1bcbd7d6a2f753be2ed401c19b63
Michael Kinnas <michaelkinnas@gmail.com>
2026-02-08 12:58:57 +0200

    restored proper main

--- fire_spreading/main.py
+++ fire_spreading/main.py
@@ -2,6 +2,7 @@ import pygame
 import sys
 import random
 from noise import pnoise2
+# import numpy as np
 
 """
 INSTRUCTIONS:
@@ -25,7 +26,7 @@ WHITE = (200, 200, 200)
 BLACK = (0, 0, 0)
 
 GREEN = (61, 99, 55)
-GREY = (186, 186, 186)
+GREY  = (186, 186, 186)
 BROWN = (56, 19, 0)
 FIRE1 = (180, 90, 20)
 FIRE2 = (255, 140, 0)
@@ -42,9 +43,6 @@ TERRAIN_BASE_PROBS = {0: 0.7, 1: 0.4, 2: 0.2}
 TERRAIN_GEN_SCALE = 30
 
 FIRE_INTESITY = 1
-FIRE_DURATION = 7  # number of updates
-FIRST_FIRE_STATE = 3
-LAST_FIRE_STATE = 9
 
 COLOR_CODES = [
     GREEN,
@@ -55,8 +53,8 @@ COLOR_CODES = [
     FIRE3,
     FIRE3,
     FIRE3,
-    # FIRE3,
-    # FIRE3,
+    FIRE3,
+    FIRE3,
     FIRE2,
     FIRE1,
     BLACK1,
@@ -64,6 +62,21 @@ COLOR_CODES = [
     BLACK3,
 ]
 
+color_codes_terrain = (GREEN, GREY, BROWN, BLACK1, BLACK2, BLACK3)
+color_codes_fire_states = (
+    FIRE1,
+    FIRE2,
+    FIRE3,
+    FIRE3,
+    FIRE3,
+    FIRE3,
+    FIRE3,
+    FIRE2,
+    FIRE1,
+)
+
+NEIGHBOR_FACTOR = 0.125
+NEIGHBOR_FACTORS = [x * NEIGHBOR_FACTOR for x in range(9)]
 
 def build_terrain_grid(
     h_cell_count, v_cell_count, terrain_types=3, gen_scale=TERRAIN_GEN_SCALE, seed=69
@@ -101,70 +114,73 @@ def build_terrain_grid(
 
 
 def ignition_prob(
-    grid,
+    state_grid,
     pos_i,
     pos_j,
-    fire_states_range,
 ):
-    neighbor_factor = 0.125
+    # neighbor_factor = 0.125
     count = 0
-
-    for i in range(pos_i - 1, pos_i + 2):
-        for j in range(pos_j - 1, pos_j + 2):
+    for i in range(max(pos_i - 1, 0), min(pos_i + 2, V_CELL_COUNT)):
+        for j in range(max(pos_j - 1, 0), min(pos_j + 2, H_CELL_COUNT)):
             if i == pos_i and j == pos_j:
                 continue
             # count number and intensity of fire
-            value = grid[(i + V_CELL_COUNT) % V_CELL_COUNT][
-                (j + H_CELL_COUNT) % H_CELL_COUNT
-            ]
-            if value >= fire_states_range[0] and value <= fire_states_range[1]:
-                # neighbor_factor += value - lower_ignition_threshold + 1
+            if state_grid[i][j] == 1:
                 count += 1
-
-    prob = 1 - (1 - neighbor_factor) ** count
-    return prob
+    
+    return NEIGHBOR_FACTORS[count]
 
 
 # fire spreading logic
-def update(grid):
-    new_grid = [[0 for _ in range(H_CELL_COUNT)] for _ in range(V_CELL_COUNT)]
+def update(terrain_grid, state_grid, fire_age_grid, max_fire_age=10):
+    new_state_grid = [row[:] for row in state_grid]
+    new_age_grid = [row[:] for row in fire_age_grid]
+    new_terrain_grid = [row[:] for row in terrain_grid]
 
-    for i in range(len(grid)):
-        for j in range(len(grid[i])):
-            if grid[i][j] < FIRST_FIRE_STATE:  # if the cell has no fire
+    # rand = random.random
+    # terrain_probs = TERRAIN_BASE_PROBS
+    # fire_intensity = FIRE_INTESITY
 
+    for i in range(V_CELL_COUNT):
+        for j in range(H_CELL_COUNT):
+            if state_grid[i][j] == 0:  # if the cell has no fire
                 # calculate ignition probability
-                ign_prob = ignition_prob(grid, i, j, fire_states_range=[3, 9])
-                if (
-                    random.random()
-                    < TERRAIN_BASE_PROBS[grid[i][j]] * ign_prob * FIRE_INTESITY
-                ):
-                    new_grid[i][j] = FIRST_FIRE_STATE
+                ign_prob = ignition_prob(state_grid, i, j)
+                if ign_prob > 0 and (random.random() < TERRAIN_BASE_PROBS[terrain_grid[i][j]] * ign_prob * FIRE_INTESITY):
+                    new_state_grid[i][j] = 1
+
+            # if in state of fire increase age of fire
+            elif state_grid[i][j] == 1:
+                # check weather last age of fire and transition to burned
+                if fire_age_grid[i][j] > max_fire_age:
+                    new_state_grid[i][j] = 2
+                    new_terrain_grid[i][j] = random.choice([3,4,5])
                 else:
-                    new_grid[i][j] = grid[i][j]
+                    new_age_grid[i][j] += 1
 
-            # if in state of fire increase to next state of fire
-            elif grid[i][j] < LAST_FIRE_STATE:
-                new_grid[i][j] = grid[i][j] + 1
-
-            # if in last state of fire increase to ash state and assign a random ash value
-            elif grid[i][j] == LAST_FIRE_STATE:
-                new_grid[i][j] += random.randint(
-                    LAST_FIRE_STATE + 0, LAST_FIRE_STATE + 2
-                )
-            else:
-                new_grid[i][j] = grid[i][j]
-    return new_grid
+    return new_state_grid, new_age_grid, new_terrain_grid
 
 
-def draw_grid(screen, grid, color_codes):
+def draw_grid(
+    screen,
+    terrain,
+    state,
+    fire_animation_state,
+    color_codes_terrain,
+    color_codes_fire_states,
+):
     screen.fill(BLACK)
 
-    for i in range(len(grid)):
-        for j in range(len(grid[i])):
+    for i in range(len(terrain)):
+        for j in range(len(terrain[i])):
+            if state[i][j] == 1: #on fire
+                # color = color_codes_fire_states[fire_animation_state[i][j]]
+                color = random.choice(color_codes_fire_states)
+            else: #not on fire
+                color = color_codes_terrain[terrain[i][j]]
             pygame.draw.rect(
                 surface=screen,
-                color=color_codes[grid[i][j]],
+                color=color,
                 rect=pygame.Rect(
                     j * CELL_SIZE,
                     i * CELL_SIZE,
@@ -177,18 +193,29 @@ def draw_grid(screen, grid, color_codes):
 def main():
     pygame.init()
     screen = pygame.display.set_mode((WIDTH, HEIGHT))
-    pygame.display.set_caption("Conway's Game of Life")
+    pygame.display.set_caption("Fire simulation")
     clock = pygame.time.Clock()
 
     paused = False
     frame_count = 0
 
-    # grid = [[0 for _ in range(H_CELL_COUNT)] for _ in range(V_CELL_COUNT)]
-    grid = build_terrain_grid(
+    terrain = build_terrain_grid(
         H_CELL_COUNT, V_CELL_COUNT, terrain_types=3, gen_scale=30, seed=420
     )
 
+    # 0 not burning, 1 burning, 2 burned
+    cell_state = [[0 for _ in range(H_CELL_COUNT)] for _ in range(V_CELL_COUNT)]
+
+    # fire age
+    fire_age = [[0 for _ in range(H_CELL_COUNT)] for _ in range(V_CELL_COUNT)]
+
+    # 0 to whatever animation
+    fire_animation_state = [
+        [0 for _ in range(H_CELL_COUNT)] for _ in range(V_CELL_COUNT)
+    ]
+
     while True:
+        # print(cell_state)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
@@ -197,7 +224,7 @@ def main():
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if pygame.mouse.get_pressed()[0]:
                     mouse_x, mouse_y = pygame.mouse.get_pos()
-                    grid[mouse_y // CELL_SIZE][mouse_x // CELL_SIZE] = 3
+                    cell_state[mouse_y // CELL_SIZE][mouse_x // CELL_SIZE] = 1
 
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE:
@@ -207,23 +234,33 @@ def main():
                         paused = True
 
                 if event.key == pygame.K_r:
-                    grid = build_terrain_grid(
+                    terrain = build_terrain_grid(
                         H_CELL_COUNT,
                         V_CELL_COUNT,
                         terrain_types=3,
                         gen_scale=30,
                         seed=69,
                     )
-
-        draw_grid(screen, grid, color_codes=COLOR_CODES)
+        # if frame_count % STEP == 0:
+        draw_grid(
+            screen=screen,
+            terrain=terrain,
+            state=cell_state,
+            fire_animation_state=fire_animation_state,
+            color_codes_terrain=color_codes_terrain,
+            color_codes_fire_states=color_codes_fire_states,
+        )
 
         if not paused:
-            # if frame_count % STEP == 0:
-            #     grid = update_generation(grid)
-
             if frame_count % STEP == 0:
-                grid = update(grid)
+                cell_state, fire_age, terrain = update(
+                    terrain_grid=terrain,
+                    state_grid=cell_state,
+                    fire_age_grid=fire_age,
+                    max_fire_age=10,
+                )
 
+        # print(cell_state)
         pygame.display.flip()
         clock.tick(FPS)
 
